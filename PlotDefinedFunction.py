896191f9951853eea93dc33885bdd6cee80f54a3
Yi Qin <qinyi_linxuan@126.com>
2021-02-23 22:14:53 -0800

    add two functions for adding colorbar

--- PlotDefinedFunction.py
+++ PlotDefinedFunction.py
@@ -24,6 +24,8 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from sklearn.metrics import r2_score
 
+from matplotlib import ticker
+
 #========================================================================================================
 def find_model_list(datadir,prefix,suffix1,suffix2,phases,exp_cntl,exp_new):
     '''
@@ -404,6 +406,60 @@ def get_intersect_withripf(exp_cntl,exp_new,prefix,suffix1,suffix2,datadir):
 
     return models_ripfs_all,cmip5_models_ripfs,cmip6_models_ripfs
 
+
+# ==========================================================================================================================
+def make_colorbar(ax, units, fh, mappable, **kwargs):
+    '''
+    Nov 23, 2020: a function to add colorbar fitted well with the figure
+    Referred from: https://github.com/pydata/xarray/issues/619
+    units: the unit of colorbar
+    fh: fontsize of colorbar label font size
+    '''
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+    import matplotlib as mpl
+
+    divider = make_axes_locatable(ax)
+    orientation = kwargs.pop('orientation', 'vertical')
+    if orientation == 'vertical':
+        loc = 'right'
+    elif orientation == 'horizontal':
+        loc = 'bottom'
+        
+    cax = divider.append_axes(loc, '5%', pad='5%', axes_class=mpl.pyplot.Axes)
+    cb = ax.get_figure().colorbar(mappable, cax=cax, orientation=orientation,extend='both')
+    cb.set_label(units,fontsize=fh)
+    cb.ax.tick_params(labelsize=fh)
+    tick_locator = ticker.MaxNLocator(nbins=9)
+    cb.locator = tick_locator
+    cb.update_ticks()
 # ==========================================================================================================================
 
+
+def add_common_colorbar(fig,im,axes,units,orientation='vertical',nbins=9,fontsize=15):
+    '''
+    Feb 12, 2021: generate a common colorbar for several specific subplots.
+    For example, subplots on the same row or the same column.
+    -----------------------
+    Inputs:
+    fig -- 
+    im -- return handle. 
+    axes --- the list for all subplots.
+    units --- unit of colorbar.
+    orientation --- direction of colorbar.
+    nbins --- number of labeled ticks. default: 9
+    fontsize --- label fontsize for colorbar. 
+    '''
+    pos1 = axes[-1].get_position() # get the original position for the last subplot 
+    if orientation == 'vertical':
+        pos2 = [pos1.x0 + pos1.width + 0.01, pos1.y0,  pos1.width / 20.0, pos1.height ]   
+    else:
+        pos2 = [pos1.x0, pos1.y0 - 0.01, pos1.width, pos1.height / 25.0]
+        
+    cbar_ax = fig.add_axes(pos2)
+    cb = fig.colorbar(im,ax=axes, orientation=orientation, cax=cbar_ax)
+    cb.set_label(units,fontsize=fontsize)
+    cb.ax.tick_params(labelsize=fontsize)
+    tick_locator = ticker.MaxNLocator(nbins=nbins)
+    cb.locator = tick_locator
+    cb.update_ticks()
 
