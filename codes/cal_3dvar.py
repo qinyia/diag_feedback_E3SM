08187888d81756ec34e99b26d4f4900668fb268b
Yi Qin <qinyi_linxuan@126.com>
2023-01-23 22:45:55 -0800

    clean extra vars and add calc_EIS func locally

--- codes/cal_3dvar.py
+++ codes/cal_3dvar.py
@@ -31,9 +31,71 @@ import genutil
 from genutil import statistics
 import numpy.ma as ma
 from global_land_mask import globe 
-import metpy.calc as mpcalc
-import cal_EIS as calEIS
  
+def calc_EIS(Ts, SLP, T700, z700):
+    '''
+    Input:
+           Ts: surface temperature (K)
+           SLP: sea level pressure (Pa)
+           T700: temperature at 700 hPa (K)
+           z700: height at 700 hPa (m)
+    Output:
+           EIS: estimated inversion strengh (K)
+           LTS: lower troposheric strength (K)
+
+    # Notes:
+    #  - surface relative humidity is assumed to be constant
+    #  - potential temperature is calcualted with respect to the 1000 hPa
+    #    reference level
+    #  - traditionally, surface air temperature is used in place of surface
+    #    temperature.
+    #  - physical constants are from Curry and Webster, Appendix B
+    #
+    # Physical Constants:
+    #  - Gas constant for water vapor = 461 J/K/kg
+    #  - Latent heat of vaporization at 273.15 K = 2.5*10^6 J/kg
+    #  - Gas constant for dry air = 287 J/K/kg
+    #  - Specific heat at constant pressure = 1004 J/K/kg
+    #
+    #References:
+    #
+    #Curry, J. A., & Webster, P. J. (1998). Thermodynamics of Atmospheres and
+    #Oceans. Elsevier.
+    #
+    #Georgakakos, K. P., & Bras, R. L. (1984). A hydrologically useful station
+    #precipitation model: 1. Formulation. Water Resources Research, 20(11),
+    #1585-1596, https://doi.org/10.1029/WR020i011p01585
+
+    #
+    #Wood, R., & Bretherton, C. S. (2006). On the relationship between
+    #stratiform low cloud cover and lower-tropospheric stability. Journal of
+    #Climate, 19(24), 6425-6432, https://doi.org/10.1175/JCLI3988.1
+
+    '''
+
+    #calculate the LCL height
+    es=6.11*np.exp(((2.5*10**6)/461)*((1/273.15)-(1/Ts))) #Clausius-Clapeyron Equation (Equation 4.23 from Curry and Webster)
+    e=es*80/100 #assume RH is 80%, as in WB06
+    Td=((1/273.15)-(461/(2.5*10**6))*np.log(e/6.11))**(-1) #form of Clausius-Clapeyron Equation
+    p_LCL=SLP*(((Ts-Td)/223.15)+1)**(-3.5) #Equation (12) from Georgakakos and Bras (1984)
+    T_LCL=Ts*(((Ts-Td)/223.15)+1)**(-1) #Equation (13) from Georgakakos and Bras (1984)
+    LCL=((287*(Ts+T_LCL)/2)/9.8)*np.log(SLP/p_LCL) #Hypsometric equation
+
+    #calculate LTS
+    theta700=T700*(1000/700)**(287/1004)
+    theta_slp=Ts*(1000./(SLP*0.01))**(287/1004)
+    LTS=theta700-theta_slp
+
+    #calculate the moist adaiabtic lapse rate at 850 hPa
+    T_bar=(Ts+T700)/2 #approximation of the temperature at 850 hPa, following WB06
+    es_bar=6.11*np.exp(((2.5*10**6)/461)*((1/273.15)-(1/T_bar))) #Clausius-Clapeyron Equation (Equation 4.23 from Curry and Webster)
+    qs_bar=0.622*es_bar/(850-es_bar) #Equation 4.37 of Curry and Webster
+    gamma_m=(9.8/1004)*(1-((1+(((2.5*10**6)*qs_bar)/(287*T_bar)))/(1+((((2.5*10**6)**2)*qs_bar)/(1004*461*(T_bar**2)))))) #Equation (5) of WB06
+
+    EIS=LTS-gamma_m*(z700-LCL); #Equation (4) of WB06
+
+    return EIS, LTS
+
 ###########################################################################
 # HELPFUL FUNCTIONS FOLLOW
 ###########################################################################
@@ -74,27 +136,8 @@ def cal_3dvar(direc_data,case_stamp,yearS,yearE,fname1,fname2,outdir,figdir,exp1
     grid = cdms.createGenericGrid(lats,lons)
 
     var3d = [\
-#               'TGCLDLWP','CLDLOW','netCRE','SWCRE','LWCRE',
-#             'CLOUDCOVER_CLUBB', 'CLOUDFRAC_CLUBB', 'RCMINLAYER_CLUBB', 'RCM_CLUBB',
-#             'RTP2_CLUBB', 'RTPTHLP_CLUBB', 'THLP2_CLUBB',
-#             'WP2_CLUBB', 'WP3_CLUBB', 'WPRCP_CLUBB', 'WPRTP_CLUBB', 'WPTHLP_CLUBB', 'WPTHVP_CLUBB','Skw',\
-             'RCMTEND_CLUBB', 'MPDLIQ','DPDLFLIQ', 'MPDW2P',
-             'DCQ','ZMDQ','MPDQ','RVMTEND_CLUBB','DYNTEQ',
-             'DCCLDLIQ','ZMDLIQ','DPDLFLIQ','MPDLIQ','RCMTEND_CLUBB',
-#             'ts', 'CLOUD', 'CLDLIQ', 'TGCLDLWP', 'RELHUM', 'T', 'Q', 'Qsfc_700', 
-#             'EIS', 'CLOUD','CLDLIQ',\
-#             'OMEGA', 'U', 'V', 'ts_tas', 'SHFLX', 'LHFLX', 'RHsfc', 'PRECT', 'rlus', 
-             'DTCOND','DTCORE', 'ZMDT', 'TTEND_CLUBB', 'MPDT', 'QRL', 'QRS', 
-#             'CONCLD', 
-#             'CMFMCDZM', 'ZMMU', 'ZMMD', 
-#             'PRECC', 
-#             'rlds', 'rlus', 'FDLCA', 'FDLA', 'FULCA', 'FULA', 'FNLCA', 'FNLA', 
-#            'QRLC', 'QRSC'
-#            'DPBLH','PBLHI','PBLHOLD', 'FRQPBL', 'ZLCL', 
-#             'EIS','DCPPBL','DTHVINV','DTINV','DZINV',
-#             'ENTEFF',#'ENTEFFN','ENTEFFP','ENTEFF_ALL','ENTEFF_ALLN','ENTEFF_ALLP',
-#             'ENTRAT',#'ENTRATN','ENTRATP','ENTRAT_ALL','ENTRAT_ALLN','ENTRAT_ALLP',
-#             'CLDLOW','TGCLDLWP','PBLH','DCPPBL','LHFLX','SHFLX','ENTRAT'
+             'ts', 'CLOUD', 'CLDLIQ', 'TGCLDLWP','EIS',
+             'CLDLOW', 'CLDMED', 'CLDHGH'
             ]
 
     var = var3d
@@ -115,52 +158,7 @@ def cal_3dvar(direc_data,case_stamp,yearS,yearE,fname1,fname2,outdir,figdir,exp1
         for svar in ['tas',svarh]:
             print(svar)
 
-            if svar == 'HMC':
-                svar_a = 'TUQ'
-                f1 = cdms.open(direc_data+fname1+'/'+svar_a+'_'+exp1+'_'+yearS_4d+'01-'+yearE_4d+'12.nc')
-                pi_raw1 = f1(svar_a)
-                f1.close()
-                f2 = cdms.open(direc_data+fname2+'/'+svar_a+'_'+exp2+'_'+yearS_4d+'01-'+yearE_4d+'12.nc')
-                ab_raw1 = f2(svar_a)
-                f2.close()
-
-                svar_b = 'TVQ'
-                f1 = cdms.open(direc_data+fname1+'/'+svar_b+'_'+exp1+'_'+yearS_4d+'01-'+yearE_4d+'12.nc')
-                pi_raw2 = f1(svar_b)
-                f1.close()
-                f2 = cdms.open(direc_data+fname2+'/'+svar_b+'_'+exp2+'_'+yearS_4d+'01-'+yearE_4d+'12.nc')
-                ab_raw2 = f2(svar_b)
-                f2.close()
-
-                pi_raw1_grd = pi_raw1.regrid(grid,regridTool='esmf',regridMethod='linear')
-                ab_raw1_grd = ab_raw1.regrid(grid,regridTool='esmf',regridMethod='linear')
-                pi_raw2_grd = pi_raw2.regrid(grid,regridTool='esmf',regridMethod='linear')
-                ab_raw2_grd = ab_raw2.regrid(grid,regridTool='esmf',regridMethod='linear')
-
-                pi_raw1_grd.setAxis(1,lats_ax)
-                pi_raw1_grd.setAxis(2,lons_ax)
-
-                print(pi_raw1_grd.shape, ab_raw1_grd.shape, pi_raw2_grd.shape, ab_raw2_grd.shape)
-
-                # calculate moisture convergence 
-                lons = pi_raw1_grd.getLongitude()[:]
-                lats = pi_raw1_grd.getLatitude()[:]
-                # Compute dx and dy spacing for use in divergence calculation
-                dx, dy = mpcalc.lat_lon_grid_deltas(lons, lats)
-                # Calculate moisture flux divergence
-                HMC_pi = np.ma.zeros((pi_raw1_grd.shape))
-                HMC_ab = np.ma.zeros((pi_raw1_grd.shape))
-                for tt in range(pi_raw1_grd.shape[0]):
-                    HMC_pi[tt,:] = mpcalc.divergence(pi_raw1_grd[tt,:], pi_raw2_grd[tt,:], dx=dx, dy=dy)
-                    HMC_ab[tt,:] = mpcalc.divergence(ab_raw1_grd[tt,:], ab_raw2_grd[tt,:], dx=dx, dy=dy)
-    
-                pi_raw_grd = cdms.asVariable(HMC_pi)
-                ab_raw_grd = cdms.asVariable(HMC_ab)
-
-                pi_raw_grd.setAxisList(pi_raw1_grd.getAxisList())
-                ab_raw_grd.setAxisList(ab_raw1_grd.getAxisList())
-
-            elif svar in ['SWCRE','LWCRE','netCRE','Skw','EIS','Qsfc_700','DYNTEQ','ts_tas','RHsfc','PRECT']:
+            if svar in ['SWCRE','LWCRE','netCRE','Skw','EIS','Qsfc_700','DYNTEQ','ts_tas','RHsfc','PRECT']:
                 if svar == 'netCRE':
                     svar_here = ['rsutcs','rsut','rlutcs','rlut']
                 elif svar == 'SWCRE':
@@ -229,8 +227,8 @@ def cal_3dvar(direc_data,case_stamp,yearS,yearE,fname1,fname2,outdir,figdir,exp1
                     cre_pi = dics['SKW_ZM_bf'][0]
                     cre_ab = dics['SKW_ZM_bf'][1]
                 elif svar == 'EIS':
-                    cre_pi,_ = calEIS.calc_EIS(dics['ts'][0], dics['psl'][0], dics['T'][0], dics['Z3'][0])
-                    cre_ab,_ = calEIS.calc_EIS(dics['ts'][1], dics['psl'][1], dics['T'][1], dics['Z3'][1])
+                    cre_pi,_ = calc_EIS(dics['ts'][0], dics['psl'][0], dics['T'][0], dics['Z3'][0])
+                    cre_ab,_ = calc_EIS(dics['ts'][1], dics['psl'][1], dics['T'][1], dics['Z3'][1])
                 elif svar == 'Qsfc_700':
                     cre_pi = dics['Q'][0].pressureRegrid(cdms.createAxis([1000]))[:,0,:,:] - dics['Q'][0].pressureRegrid(cdms.createAxis([700]))[:,0,:,:]
                     cre_ab = dics['Q'][1].pressureRegrid(cdms.createAxis([1000]))[:,0,:,:] - dics['Q'][1].pressureRegrid(cdms.createAxis([700]))[:,0,:,:]
