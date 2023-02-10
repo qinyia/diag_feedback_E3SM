0bbb124acfdfbf0b877e526847833abdb3928b99
Yi Qin <qinyi_linxuan@126.com>
2023-02-10 11:15:58 -0800

    add demo exps for compy machine

--- cases_lookup.py
+++ cases_lookup.py
@@ -1,21 +1,13 @@
 
 def get_lutable(version,exp):
     lu_table = {
-    'v1_coupled':[
-    'None',
-    'None',
-    ],
-    'v2_coupled':[
-    'None',
-    'None',
-    ],
     'v1':[
     '20211208.F2010C5-CMIP6-LR.IC.ne30_oECv3.compy.1080',
     '20211210.F2010C5-CMIP6-LR.IC.p4Ka.ne30_oECv3.compy.1080',
     ],
-    'v2':[
-    '20211208.v2.F2010-CICE.IC.ne30pg2_EC.compy',
-    '20211210.v2.F2010-CICE.IC.p4Ka.ne30pg2_EC.compy',
+    'v2test':[
+    '20221013.v2.F2010-CICE.IC.back.clubb.g1.1.ne30pg2_EC.compy',
+    '20221013.v2.F2010-CICE.IC.back.clubb.g1.1.p4Ka.ne30pg2_EC.compy',
     ],
     }
 
