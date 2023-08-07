87f3c5b180cc2af64e0a6c440fe164f86b7ed1a6
Yi Qin <qinyi_linxuan@126.com>
2023-08-07 15:37:45 -0700

    converted to xarray version

--- codes/cal_global_radiation_feedback_E3SM.py
+++ codes/cal_global_radiation_feedback_E3SM.py
@@ -19,9 +19,7 @@
 
 #IMPORT STUFF:
 #=====================
-import cdms2 as cdms
-import cdutil
-import MV2 as MV
+import xarray as xr
 import numpy as np
 import pylab as pl
 import matplotlib as mpl
@@ -33,18 +31,18 @@ import cartopy
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
-import cdtime
-import time
-import genutil
 import numpy.ma as ma
-from genutil import statistics
+sys.path.append('../')
+import cases_lookup as CL
+import PlotDefinedFunction as PDF
+
 
 ########## MAIN SUBROUTINE STARTS HERE ....
 def Global_RadFeedback(direc_data,case_stamp,yearS,yearE,fname1,fname2,outdir,exp1,exp2):
 
-    if os.path.isfile(outdir+'global_mean_features_'+case_stamp+'.csv'):
-        print('Global_RadFeedback is done.')
-        return 
+    #if os.path.isfile(outdir+'global_mean_features_'+case_stamp+'.csv'):
+    #    print('Global_RadFeedback is done.')
+    #    return 
 
     yearS_4d = "{:04d}".format(yearS)
     yearE_4d = "{:04d}".format(yearE)
@@ -101,12 +99,12 @@ def Global_RadFeedback(direc_data,case_stamp,yearS,yearE,fname1,fname2,outdir,ex
         
                     if svar in var:
                         print(svar)
-                        f1 = cdms.open(direc_data+fname1+'/'+svar+'_'+exp1+'_'+yearS_4d+'01-'+yearE_4d+'12.nc')
-                        pi_raw = f1(svar)
+                        f1 = xr.open_dataset(direc_data+fname1+'/'+svar+'_'+exp1+'_'+yearS_4d+'01-'+yearE_4d+'12.nc')
+                        pi_raw = f1[svar]
                         f1.close()
     
-                        f2 = cdms.open(direc_data+fname2+'/'+svar+'_'+exp2+'_'+yearS_4d+'01-'+yearE_4d+'12.nc')
-                        ab_raw = f2(svar)
+                        f2 = xr.open_dataset(direc_data+fname2+'/'+svar+'_'+exp2+'_'+yearS_4d+'01-'+yearE_4d+'12.nc')
+                        ab_raw = f2[svar]
                         f2.close()
     
                         # reverse lev direction
@@ -117,13 +115,9 @@ def Global_RadFeedback(direc_data,case_stamp,yearS,yearE,fname1,fname2,outdir,ex
                         print(pi_raw.shape)
                         print(ab_raw.shape)
    
-                        dic_all[svar+'_ano'] = ab_raw - pi_raw
+                        dic_all[svar+'_ano'] = xr.DataArray(ab_raw - pi_raw, coords=pi_raw.coords)
                         dic_all[svar+'_pi'] = pi_raw
                         dic_all[svar+'_ab'] = ab_raw
-
-                        dic_all[svar+'_ano'].setAxisList(pi_raw.getAxisList())
-                        dic_all[svar+'_pi'].setAxisList(pi_raw.getAxisList())
-                        dic_all[svar+'_ab'].setAxisList(pi_raw.getAxisList())
 
                         del(pi_raw, ab_raw)
                     else:
@@ -166,7 +160,8 @@ def Global_RadFeedback(direc_data,case_stamp,yearS,yearE,fname1,fname2,outdir,ex
                 dic_all['FSNTCLR_ab'] = dic_all['rsntcs_ab']
                 dic_all['FLNTCLR_ab'] = -1.*dic_all['rlutcs_ab']
                 
-                AXL = dic_all['rlutcs_pi'].getAxisList()
+                # Define new time coordinate
+                newtime = pd.date_range(start='1850-01-01', periods=dic_all['rsnt_pi'].shape[0], freq='MS')
         
                 thesevars = []
                 thesevars = np.append(thesevars, ['ts','SWCRE','LWCRE','netCRE','SWCLR','LWCLR','FTOA','FSNT','FLNT','FTOACLR','FSNTCLR','FLNTCLR'])  
@@ -177,27 +172,29 @@ def Global_RadFeedback(direc_data,case_stamp,yearS,yearE,fname1,fname2,outdir,ex
                 ### get monthly climatology
                 for ivar,svar in enumerate(thesevars):
                     print('here we are processing ',svar)
+
+                    dic_all[svar+'_ano'] = dic_all[svar+'_ano'].assign_coords({'time':("time",newtime)})
   
-                    # Mar 11: add setting time bounds to avoid the 'Nontype error from time.py' while get annual mean using cdutil.YEAR
-                    cdutil.setTimeBoundsMonthly(dic_all[svar+'_ano'],1)
-    
                     # get regression onto global mean surface air temperature anomaly
                     # 0. get annual mean of each variable
-                    dic_new[svar+'_ano_ann'] = cdutil.YEAR(dic_all[svar+'_ano'])
+                    dic_new[svar+'_ano_ann'] = PDF.weighted_annual_mean(dic_all[svar+'_ano'].time,dic_all[svar+'_ano'])
                 
                     # 1. get global mean surface air temperature series 
                     if svar=='ts':
-                        dic_new[svar+'_ano_ann_gavg'] = cdutil.averager(dic_new[svar+'_ano_ann'],axis='xy',weights='weighted')
+                        dic_new[svar+'_ano_ann_gavg'] = PDF.area_averager(dic_new[svar+'_ano_ann'])
 
-                    # get global and climatological mean 
-                    #dic_new[svar+'_ano_gaavg'] = cdutil.averager(MV.average(dic_new[svar+'_ano_ann'],axis=0), axis='xy', weights='weighted')
-                    dic_new[svar+'_ano_gaavg'] = cdutil.averager(MV.average(dic_all[svar+'_ano'],axis=0), axis='xy', weights='weighted')
+                    # get global and climatological mean - Note: average of annual mean and average of all monthly data result in slightly different results.
+                    #dic_new[svar+'_ano_gaavg'] = PDF.area_averager((dic_new[svar+'_ano_ann'].mean(axis=0)))
+                    dic_new[svar+'_ano_gaavg'] = PDF.area_averager((dic_all[svar+'_ano'].mean(axis=0)))
 
                     # 2. regression on ts anomaly loop over each variable
-                    dic_new[svar+'_ano_slope'],dic_new[svar+'_ano_intercept'] = statistics.linearregression(dic_new[svar+'_ano_ann'],x=dic_new['ts_ano_ann_gavg'])
+                    dic_new[svar+'_ano_slope'],dic_new[svar+'_ano_intercept'] = PDF.linearregression_nd(dic_new[svar+'_ano_ann'],x=np.reshape(dic_new['ts_ano_ann_gavg'].values, (dic_new[svar+'_ano_ann'].shape[0],1,1)))
 
                     # 3. get global mean of slope
-                    dic_new[svar+'_ano_slope_gavg'] = cdutil.averager(dic_new[svar+'_ano_slope'],axis='xy',weights='weighted')
+                    dic_new[svar+'_ano_slope'] = xr.DataArray(dic_new[svar+'_ano_slope'], coords=dic_all[svar+'_ano'][0,:,:].coords)
+                    dic_new[svar+'_ano_intercept'] = xr.DataArray(dic_new[svar+'_ano_intercept'], coords=dic_all[svar+'_ano'][0,:,:].coords)
+
+                    dic_new[svar+'_ano_slope_gavg'] = PDF.area_averager(dic_new[svar+'_ano_slope'])
 
                     ### get global mean climatology
                     dic_new[svar+'_ano_gaavg_perK'] = dic_new[svar+'_ano_gaavg']/dic_new['ts_ano_gaavg']
@@ -211,7 +208,7 @@ def Global_RadFeedback(direc_data,case_stamp,yearS,yearE,fname1,fname2,outdir,ex
                         ano_out = dic_new[svar+'_ano_gaavg']
                         ano_perK_out = dic_new[svar+'_ano_gaavg_perK']
 
-                    tmp = pd.DataFrame([[svar,str(np.round(ano_out,3)),str(np.round(ano_perK_out,3))]],columns=['varname','anomaly','anomaly_perK'])
+                    tmp = pd.DataFrame([[svar,str(np.round(ano_out.values,3)),str(np.round(ano_perK_out.values,3))]],columns=['varname','anomaly','anomaly_perK'])
 
                     print(tmp)
                     df_gavg = pd.concat([df_gavg,tmp])
@@ -220,4 +217,26 @@ def Global_RadFeedback(direc_data,case_stamp,yearS,yearE,fname1,fname2,outdir,ex
                 print(df_gavg)
     
                 df_gavg.to_csv(outdir+'global_mean_features_'+case_stamp+'.csv')
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+if __name__ == "__main__":
+
+    #direc_data = '/compyfs/qiny108/diag_feedback_E3SM_postdata/'
+    direc_data = '/compyfs/qiny108/colla/diag_feedback_E3SM_postdata/'
+
+    case_stamp = 'v2test'
+    yearS = 2
+    yearE = 3
+    fname1,_,_ = CL.get_lutable(case_stamp,'amip')
+    fname2,_,_ = CL.get_lutable(case_stamp,'amip4K')
+    outdir = './'
+    figdir = './'
+    exp1 = 'FC5'
+    exp2 = 'FC5_4K'
+    
+    Global_RadFeedback(direc_data,case_stamp,yearS,yearE,fname1,fname2,outdir,exp1,exp2)
+
 
